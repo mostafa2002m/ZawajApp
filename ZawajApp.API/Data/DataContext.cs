2db0302fa9e8e8685e3d42f402503f7fb41b9d37
Ibrahim <mostafa2002m@gmail.com>
2019-11-19 10:10:29 +0300

    initial commit

---
+++ ZawajApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZawajApp.API.Models;
+
+namespace ZawajApi.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) {}
+        public DbSet<Value> Values { get; set; }
+    }
+
+}
